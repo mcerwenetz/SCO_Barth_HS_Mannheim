36d78356ea184ed333ccdd01bc374365291a2d44
Student 1824006 <n1824006@x2go>
2023-03-31 09:09:03 +0200

    queue working

--- b1/queue.c
+++ b1/queue.c
@@ -13,18 +13,34 @@ struct Queue{
 	int size;
 };
 
-void enter(void* data, struct Queue queue){
+void enter(void* data, struct Queue* queue){
 	struct Node* new_node = (struct Node*) malloc(sizeof(struct Node*));
 	new_node->data=data;
 	new_node->next = NULL;
-	queue.tail=new_node;
+	//if queue is empty, initialize
+	if(queue->tail == NULL && queue->head == NULL){
+			queue->size=0;
+	}
+	queue->size++;
+	queue->tail=new_node;
+	//first and only node means head is the tail
+	if(queue->size == 1){
+		queue->head = queue->tail;
+	}else{
+		queue->head->next = new_node;
+	}
 }
 
-void* leave(struct Queue queue){
-	struct Node* to_delete = queue.head;
+void* leave(struct Queue* queue){
+	struct Node* to_delete = queue->head;
 	void* data = to_delete->data;
-	queue.head = queue.head->next;
+	queue->head = queue->head->next;
 	free(to_delete);
+	queue->size--;
+	//if last element deleted, tail is head is NULL
+	if(queue->size == 0){
+		queue->tail = NULL;
+	}
 	return data;
 }
 
@@ -36,10 +52,13 @@ int isEmpty(struct Queue queue){
 int main(){
 		struct Queue queue;
 		int to_enter = 10;
+		int to_enter_second = 20;
 		void* enter_void = &to_enter;
-		enter(enter_void, queue);
-		void* data = leave(queue);
+		enter(enter_void, &queue);
+		enter(&to_enter_second, &queue);
+		void* data = leave(&queue);
 		int data_in_int = *(int*) data;
-		printf("%d", data_in_int);
+		printf("%d\n", data_in_int);
+		printf("%d\n", *(int*)  leave(&queue));
 }
 
