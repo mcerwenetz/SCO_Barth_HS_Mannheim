11dca9e14cb642802390ed8516122bb31af8c2ce
Student 1824006 <n1824006@x2go>
2023-04-02 12:30:25 +0200

    segfault at cmdq when printing
    looks like strdup does not duplicate so well
    address at duplication not readable
    maybe because of null termination of first string?

--- b1/cmdq.c
+++ b1/cmdq.c
@@ -1,9 +1,42 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "queue.h"
 
 int main(){
 	char input[100];
 	fgets(input, sizeof(input), stdin);
+	const int stringsize = strlen(input);
+	char stri[stringsize];
+	strcpy(stri, input);
+
+		
+	char* token = strtok(input, " ");
 	
 	
+	int counter=0;
+		
+	while(token != NULL){
+		counter++;
+		token = strtok(NULL, " ");
 	}
+
+	char* input_strings[counter];
+	
+	
+	token = strtok(stri, " ");
+	int i =0;
+	while(token != NULL){
+		input_strings[i] = strdup(token);
+		i++;
+		token = strtok(NULL, " ");
+	}
+
+
+	for (int i=0; i <= counter; i++){
+		printf("%s", input_strings[i]);
+	}
+	
+	free(input_strings);
+	
+}
